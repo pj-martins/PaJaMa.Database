bfba23f0f6689ebfd609d6ee7a85840a7e86cb3e
PJ Martins <pj_martins@outlook.com>
2018-06-21 07:52:10 -0700

    access db

---
+++ Library/DataSources/AccessDataSource.cs
@@ -0,0 +1,34 @@
+﻿using PaJaMa.Database.Library.DatabaseObjects;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaJaMa.Database.Library.DataSources
+{
+	public class AccessOdbcDataSource : OdbcDataSource
+	{
+		public AccessOdbcDataSource(string connectionString) : base(connectionString)
+		{
+		}
+
+		private List<ColumnType> _columnTypes;
+		public override List<ColumnType> ColumnTypes
+		{
+			get
+			{
+				if (_columnTypes == null)
+				{
+					_columnTypes = new List<ColumnType>();
+                    _columnTypes.Add(new ColumnType("VARCHAR", DataType.VarChar, ""));
+                    _columnTypes.Add(new ColumnType("INTEGER", DataType.Integer, "0"));
+                    _columnTypes.Add(new ColumnType("COUNTER", DataType.Integer, "0"));
+				}
+				return _columnTypes;
+			}
+		}
+	}
+}
